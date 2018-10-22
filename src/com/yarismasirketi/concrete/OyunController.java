c2a51f942473804395d4a7210bb21ebdbb69f880
yunusbuluttopuz <yunusbuluttopuz@gmail.com>
2018-10-22 20:39:33 +0300

    OyunController class'ina sorulariYarat ve yarismaciYarat method'lari eklendi

--- src/com/yarismasirketi/concrete/OyunController.java
+++ src/com/yarismasirketi/concrete/OyunController.java
@@ -5,5 +5,35 @@ import java.util.Scanner;
 
 public class OyunController {
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	private Yarismaci yarismaciYarat() {
+		Scanner keyboardInput = new Scanner(System.in);
+		System.out.println("Yarismacinin ismini, memleketini ve yasini sirasiyla giriniz\n(Her giriden sonra enter'a basin)");		
+		Yarismaci yeniYarismaci = new Yarismaci(keyboardInput.nextLine(), keyboardInput.nextLine(), keyboardInput.nextInt());
+		return yeniYarismaci;
+		
+	}	
+	
+	private Soru[] sorulariYarat() {
+		Soru[] sorular = new Soru[SoruListesi.sorular.length];
+		for(int i = 0; i < SoruListesi.sorular.length; i++) {
+			sorular[i] = new Soru(SoruListesi.sorular[i], SoruListesi.siklar[i], SoruListesi.cevaplar[i]);
+		}		
+		return sorular;
+	}
 
 }
